c958b03cae4f7085069434bf4931d7f660edb4a1
PuccinelliRaphael <raphaelpuccinelli@gmail.com>
2016-02-22 21:56:42 +0100

    test

--- Assignment3/src/Heap.java
+++ Assignment3/src/Heap.java
@@ -12,62 +12,71 @@ public class Heap<E extends Comparable<E>>{
     
     
     public E findMin(){
+        //is known that the min element in a heap is the root
         return arr[0];
     }
+
     public E removeMin(){
-        // This works but is way too slow!
+        //is known that the min element in a heap is the root
         int minix = 0;
-//        for(int i = 1;i<size;i++)
-//            if (arr[i].compareTo(arr[minix]) < 0)
-//                minix = i;
-        
+        //storing value in a variable
         E min = arr[minix];
-        
-//        int i;
-//        for(i = minix;i<size-1;i++)
-//            arr[i]=arr[i+1];
-//        arr[size-1]=null;
+        //checking if there are elements in the heap
         if (!(arr[0] == null)) {
-
-
+            //shifting the root(min) with the last element
             E temp = arr[size - 1];
             arr[minix] = arr[size - 1];
+            //deleting the min element
             arr[size - 1] = null;
+            //decreasing the value for the quantity of elements in the heap
             size--;
 
-
-            int childLx = 0 * 2 + 1;
-            int childRx = 0 * 2 + 2;
+            //is known that the child of the root is at the indexes 1 and 2
+            int childLx = 1;
+            int childRx = 2;
+            //variable for the smallest child index
             int minChild = 0;
-
+            //if the children are not null
             if (!(arr[childLx] == null) && !(arr[childRx] == null)) {
+                //checking which children is the smallest and assign index to the variable
                 if (arr[childLx].compareTo(arr[childRx]) > 0) minChild = childRx;
                 else if (arr[childLx].compareTo(arr[childRx]) < 0) minChild = childLx;
                 else minChild = childLx;
+                //checking if there is just the left child
             } else if (arr[childLx] != null && arr[childRx] == null) {
+                //assigning its index to the variable
                 minChild = childLx;
-            } else return arr[0];
-
+                //if there is no children then just return the value of the root
+            } else return min;
+            //loop to downheap elements to satisfy the heap invariant
             while (arr[minChild].compareTo(arr[minix]) < 0) {
+                //shifting elements to satisfy heap invariant
                 E temp1 = arr[minix];
                 arr[minix] = arr[minChild];
                 arr[minChild] = temp1;
+                //once shifted assign new index location of element
                 minix = minChild;
 
-
+                //finding its new children
                 childLx = minix * 2 + 1;
                 childRx = minix * 2 + 2;
+                //checking if the left child is not out of bounds(being a heap means that the binary tree is complete and if there is nothing in the
+                //left child so consequently there will be nothing in the right child)
                 if (childLx <= size) {
+                    //checking if children are not null
                     if (!(arr[childLx] == null) && !(arr[childRx] == null)) {
+                        //checking for the smallest child and assign its index to the variable
                         if (arr[childLx].compareTo(arr[childRx]) > 0) minChild = childRx;
                         else if (arr[childLx].compareTo(arr[childRx]) < 0) minChild = childLx;
                         else minChild = childLx;
+                        //if there is just the left child then it is its index we are looking for
                     } else if (arr[childLx] != null && arr[childRx] == null) {
                         minChild = childLx;
-                    } else ;
+                    }
                 }
             }
         }
+        //returning value of deleted element
             return min;
 
     }
@@ -75,33 +84,38 @@ public class Heap<E extends Comparable<E>>{
     
     
     public void add(E elem){
+        //checking if array is full. If so create an array which has double size and transfer all elements to there
         if (size >= arr.length){
             E[] newArr = (E[]) new Comparable[size*2];
             for (int i = 0;i<arr.length;i++) newArr[i] = arr[i];
             arr = newArr;
         }
-
+        //adding element to the end of the array
         arr[size] = elem;
+        //increasing size for the next element
         size++;
+        //variable for the parent index
         int parentIndex = 0;
+        //variable to keep track of the element's index
         int elemIndex= size -1;
-        // Something more should probably be done here?
+        //if array is not null
         if(size > 0) {
+            //finding the index of the parent
             if (((size-1) % 2 == 0)) {
                 parentIndex = ((size-1) - 2) / 2;
             } else {
                 parentIndex = ((size-1) - 1) / 2;
             }
-
         }
+        //loop to upheap element so it will satisfy the heap invariant
         while((!(parentIndex < 0)) && elem.compareTo(arr[parentIndex]) < 0){
+            //shifting elements
             E temp = arr[parentIndex];
             arr[parentIndex] = arr[elemIndex];
             arr[elemIndex] = temp;
+            //storing the new index of the element
             elemIndex = parentIndex;
-
-
-
+            //checking for new parents
             if((elemIndex%2==0)){
                 parentIndex = ((elemIndex) - 2) / 2;
             }else{
--- Assignment3/src/Test.java
+++ Assignment3/src/Test.java
@@ -7,21 +7,28 @@ public class Test {
     Heap test = new Heap();
         test.add(8);
         test.add(6);
-      test.add(4);
+        test.add(4);
         test.add(1);
-       test.add(7);
+        test.add(7);
+        test.add(5);
+        test.add(3);
+        test.add(8);
+        test.add(6);
+        test.add(4);
+        test.add(1);
+        test.add(7);
         test.add(5);
         test.add(3);
 
-        test.removeMin();
-        test.removeMin();
-        test.removeMin();
-        test.removeMin();
-        test.removeMin();
-        test.removeMin();
-        test.removeMin();
-        test.removeMin();
-        test.removeMin();
+//        test.removeMin();
+//        test.removeMin();
+//        test.removeMin();
+//        test.removeMin();
+//        test.removeMin();
+//        test.removeMin();
+//        test.removeMin();
+//        test.removeMin();
+//        test.removeMin();
 
 
 
